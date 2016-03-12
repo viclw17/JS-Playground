c980540d7439306c768761f04ce432648c4d79b1
Victor Li <victor.li@archiactinteractive.com>
2016-03-11 16:07:56 -0800

    update files

---
+++ simple-lighting-model/README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/tutsplus/pen/RPYbqp.
+
+ This is the final result of part 3 of TutsPlus tutorial on writing graphics shaders.
+
+Forked from [Omar Shehata](http://codepen.io/omarshe7ta/)'s Pen [Simple Lighting Model](http://codepen.io/omarshe7ta/pen/VLxKzL/).
---
+++ simple-lighting-model/js/index.js
@@ -0,0 +1,76 @@
+//@author Omar Shehata. 2015.
+//We are loading the Three.js library from the cdn here: http://cdnjs.com/libraries/three.js/
+var scene;
+var camera;
+var renderer;
+
+function scene_setup(){
+  //This is all code needed to set up a basic ThreeJS scene
+
+  //First we initialize the scene and our camera
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  //We create the WebGL renderer and add it to the document
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+}
+
+//Initialize the Threejs scene
+scene_setup();
+
+//Pull the shader code from the html
+var shaderCode = document.getElementById("fragShader").innerHTML;
+
+//Our textures are up on github
+var textureURL = "https://raw.githubusercontent.com/tutsplus/Beginners-Guide-to-Shaders/master/Part3/images/blocks.JPG"
+var normalURL = "https://raw.githubusercontent.com/tutsplus/Beginners-Guide-to-Shaders/master/Part3/normal_maps/blocks_normal.JPG"
+
+
+THREE.ImageUtils.crossOrigin = '';//Allows us to load an external image
+
+//Load in the texture and the normal
+var texture = THREE.ImageUtils.loadTexture(textureURL);
+var normal = THREE.ImageUtils.loadTexture(normalURL);
+
+//Set up the uniforms we'll send to our share
+//More info on uniform types: http://threejs.org/docs/#Reference/Materials/ShaderMaterial
+var uniforms = {
+  tex : {type:'t',value:texture},//The texture
+  norm: {type:'t',value:normal},//Normal
+  res : {type: 'v2',value:new THREE.Vector2(window.innerWidth,window.innerHeight)},//Keeps the resolution
+  light: {type: 'v4',value:new THREE.Vector4()}//Our light source, we will use the 3 numbers as have x,y and height away from the screen. 4th value is whether the light is on or not
+}
+//We stick our shader onto a 2d plane big enough to fill the screen
+var material = new THREE.ShaderMaterial({uniforms:uniforms,fragmentShader:shaderCode})
+var geometry = new THREE.PlaneGeometry( 10, 10 );
+var sprite = new THREE.Mesh( geometry,material );
+//Add it to the scene
+scene.add( sprite );
+//Move the camera back so we can see it
+camera.position.z = 2;
+
+uniforms.light.value.z = 0.3;//How high up our light source should be
+
+uniforms.light.value.w = 1.0;//Turn light on
+
+//Render everything!
+function render() {
+  requestAnimationFrame( render );
+  renderer.render( scene, camera );
+}
+render();
+
+document.onmousemove = function(event){
+  //Update the light source to follow our mouse
+  uniforms.light.value.x = event.clientX; 
+  uniforms.light.value.y = event.clientY; 
+}
+
+document.onmousedown = function(event){
+  //Toggle lighting system
+  if( uniforms.light.value.w)
+    uniforms.light.value.w = 0.0;
+  else uniforms.light.value.w = 1.0;
+}
